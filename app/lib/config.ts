ceaca6d330315b7df02686fdfd9cc21e39ca84bf
alex h <alextheastro@gmail.com>
2026-02-06 16:10:16 -0800

    update

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,7 +1,7 @@
 export const X_CONFIG = {
   CLIENT_ID: process.env.CLIENT_ID!,
   CLIENT_SECRET: process.env.CLIENT_SECRET!,
-  REDIRECT_URI: 'http://localhost:3000/api/auth/callback',
+  REDIRECT_URI: 'http://xbookmarks.chat/api/auth/callback',
   AUTH_URL: 'https://twitter.com/i/oauth2/authorize',
   TOKEN_URL: 'https://api.twitter.com/2/oauth2/token',
   USER_INFO_URL: 'https://api.twitter.com/2/users/me',
